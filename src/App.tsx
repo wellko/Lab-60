7278c79ae751d18884715c48844bbed9ccfd3612
Kwon Valerii <wellko24@gmail.com>
2022-11-21 16:59:14 +0600

    added all components in container

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import PostForm from "./Components/PostForm/PostForm";
+import MessageBlock from "./Components/MessageBlock/MessageBlock";
 
 function App() {
   return (
     <div className="App">
+      <PostForm/>
+        <MessageBlock/>
     </div>
   );
 }
